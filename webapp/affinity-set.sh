144973e856a6ae2d6ecf17fa8dd968a2d38a9eb7
Ubuntu <isucon@isucon6-qualifier.rfowutj43mfebb0qsdshxpcrng.lx.internal.cloudapp.net>
2016-09-18 12:46:26 +0900

    affinity.sh

---
+++ webapp/affinity-set.sh
@@ -0,0 +1,13 @@
+NOW=0
+
+for i in `ps ax | grep gunicorn | grep -v grep | sort -k1 -n | cut -c 1-5`
+do
+if [ $NOW -eq 0 ]
+then
+taskset -p -c 0 $i
+NOW=1
+else
+taskset -p -c 1 $i
+NOW=0
+fi
+done
