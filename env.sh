1e6b3aadb3a6ba812f822bc140ae386d18fa10b2
Ubuntu <isucon@isucon6-qualifier.rfowutj43mfebb0qsdshxpcrng.lx.internal.cloudapp.net>
2016-09-18 11:45:48 +0900

    [Oops] Changed on server.

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
-PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:$PATH
-GOPATH=/home/isucon/gocode
+export PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:$PATH
+export GOPATH=/home/isucon/gocode
 
-ISUDA_DB_USER=isucon
-ISUDA_DB_PASSWORD=isucon
+export ISUDA_DB_USER=isucon
+export ISUDA_DB_PASSWORD=isucon
 
-ISUTAR_DB_USER=isucon
-ISUTAR_DB_PASSWORD=isucon
+export ISUTAR_DB_USER=isucon
+export ISUTAR_DB_PASSWORD=isucon
